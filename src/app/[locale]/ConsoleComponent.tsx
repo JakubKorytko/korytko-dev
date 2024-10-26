616c7de94890aba76f61ee79b42c36b7c89bd33c
Jakub Korytko <jakub@korytko.me>
2024-10-26 12:45:08 +0200

    Add console header buttons

--- src/app/[locale]/ConsoleComponent.tsx
+++ src/app/[locale]/ConsoleComponent.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './ConsoleComponent.module.scss';
+import closeIcon from '../../../public/icons/close.svg';
+import minimizeIcon from '../../../public/icons/minimize.svg';
+import maximizeIcon from '../../../public/icons/maximize.svg';
 
 function ConsoleComponent() {
   const sections: Record<string, string> = {
@@ -22,7 +26,17 @@ function ConsoleComponent() {
         <div className={styles['console-header-name']}>
           <h4>jakub@korytko.dev: ~</h4>
         </div>
-        <div className={styles['console-header-buttons']} />
+        <div className={styles['console-header-buttons']}>
+          <button type="button" className={`${styles['header-button']}`}>
+            <Image src={minimizeIcon} alt="Minimize console button" className={`h-full w-full ${styles['filter-BEBEBE']}`} />
+          </button>
+          <button type="button" className={`${styles['header-button']}`}>
+            <Image src={maximizeIcon} alt="Maximize console button" className={`h-full w-full ${styles['filter-BEBEBE']}`} />
+          </button>
+          <button type="button" className={`${styles['header-button']}`}>
+            <Image src={closeIcon} alt="Close console button" className={`h-full w-full ${styles['filter-C77777']}`} />
+          </button>
+        </div>
         <nav className={styles['console-header-links']}>
           <ul className="overflow-hidden pb-1 pl-1">
             {links}
