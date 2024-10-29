bc96b33b9c32504bc508fe08386d2f7a16a62c58
Jakub Korytko <jakub@korytko.me>
2024-10-29 18:16:17 +0100

    Make ConsoleComponent draggable

--- src/app/[locale]/ConsoleComponent.tsx
+++ src/app/[locale]/ConsoleComponent.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Image from 'next/image';
+import Draggable from 'react-draggable';
 import styles from './ConsoleComponent.module.scss';
 import ConsoleComponentHeaderLinks from '@/app/[locale]/ConsoleComponentHeaderLinks';
 import minimizeIcon from '../../../public/icons/minimize.svg';
@@ -20,40 +21,43 @@ function ConsoleComponent() {
   };
 
   return (
-    <dialog open className={`${styles['console-component']} flex flex-col`}>
-      <header className={styles['console-header']}>
-        <div className={styles['console-header-name']}>
-          <h4>
-            jakub@korytko.dev: ~
-          </h4>
-        </div>
-        <div className={styles['console-header-buttons']}>
-          <button type="button" className={`${styles['header-button']}`}>
-            <Image
-              src={minimizeIcon}
-              alt="Minimize console button"
-              className={`h-full w-full ${styles['filter-BEBEBE']}`}
-            />
-          </button>
-          <button type="button" className={`${styles['header-button']}`}>
-            <Image
-              src={maximizeIcon}
-              alt="Maximize console button"
-              className={`h-full w-full ${styles['filter-BEBEBE']}`}
-            />
-          </button>
-          <button type="button" className={`${styles['header-button']}`}>
-            <Image
-              src={closeIcon}
-              alt="Close console button"
-              className={`h-full w-full ${styles['filter-C77777']}`}
-            />
-          </button>
-        </div>
-        <ConsoleComponentHeaderLinks sections={sections} />
-      </header>
-      <div className={`${styles['console-content']} flex-grow`} />
-    </dialog>
+    <Draggable bounds="parent" handle={`.${styles['console-header-handler']}`}>
+      <dialog open className={`${styles['console-component']} flex flex-col`}>
+        <header className={styles['console-header']}>
+          <div className={`${styles['console-header-handler']} absolute w-full h-full z-0 left-0 top-0`} />
+          <div className={styles['console-header-name']}>
+            <h4 className="z-10">
+              jakub@korytko.dev: ~
+            </h4>
+          </div>
+          <div className={styles['console-header-buttons']}>
+            <button type="button" className={`${styles['header-button']}`}>
+              <Image
+                src={minimizeIcon}
+                alt="Minimize console button"
+                className={`h-full w-full ${styles['filter-BEBEBE']}`}
+              />
+            </button>
+            <button type="button" className={`${styles['header-button']}`}>
+              <Image
+                src={maximizeIcon}
+                alt="Maximize console button"
+                className={`h-full w-full ${styles['filter-BEBEBE']}`}
+              />
+            </button>
+            <button type="button" className={`${styles['header-button']}`}>
+              <Image
+                src={closeIcon}
+                alt="Close console button"
+                className={`h-full w-full ${styles['filter-C77777']}`}
+              />
+            </button>
+          </div>
+          <ConsoleComponentHeaderLinks sections={sections} />
+        </header>
+        <div className={`${styles['console-content']} flex-grow`} />
+      </dialog>
+    </Draggable>
   );
 }
 
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   return (
     <div className="w-full h-full flex flex-col">
       <TaskBar />
-      <div className="grow relative flex justify-center items-center">
+      <div className="grow relative flex justify-center items-center overflow-hidden">
         <ConsoleComponent />
       </div>
     </div>
